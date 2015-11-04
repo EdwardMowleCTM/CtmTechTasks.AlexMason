ec6a92027e669c5e4623c179cb4bf3005a00e5ed
SevenInitiative <edward.mowle@gmail.com>
2015-11-04 11:03:06 +0000

    Init commit.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    sync.init({
+        server: "./"
+    });
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch(["js/**/*.js", "*.html"]).on('change', sync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(sync.stream());
+});
+
+gulp.task('default', ['serve']);
