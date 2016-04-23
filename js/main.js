9810e589a66ae5f81ccb9b7f411b17de1130dd46
alexmason355 <alexmason369@gmail.com>
2016-04-23 10:22:40 +0100

    Task5/6(unfinished): optional sorting in place but CSS media not taking effect

--- js/main.js
+++ js/main.js
@@ -13,26 +13,51 @@ function sortJsonByAnnualFee(a, b) {
 
 //Begins when the page has finished loading, makes sure json data can be loaded
 $(document).ready(function() {
-    var index = 1; //Iterates to give unique ID's and classes to certain elements
-    $.getJSON('cards.json', function(data) {
-        data = $(data).sort(sortJsonByCashback);
-        $.each(data, function(i, creditCard) {
-            //Inserts credit card name and apr into accordion header
-            $('#accordion').append('<h3 id="header' + index + '"><b><span class="creditCard-name">' + creditCard.name +
-            '</span><span class="apr">' + creditCard.apr + '% APR</span></b></h3>');
-            //Inserts credit card image, information an cashback into accordion body
-            $('#header' + index).after('<div class="accordionContent"><img src="/img/' + creditCard.code.toLowerCase() + '.png"/>' +
-                '<p class="information">' + creditCard.information + '</p>' +
-                '<p class="cashback" id="cashback-sign' + index + '">Cashback</p>' +
-                '<p class="cashbackValue" id="cashback-value' + index + '">£' + creditCard.cashback + '</p>' +
-                '<p class="aprAccBody" id="apr-sign' + index + '">APR</p>' +
-                '<p class="aprAccBodyValue" id="apr-value' + index + '">' + creditCard.apr + '%</p>' +
-                '<p class="annFee" id="annFee-sign' + index + '">Annual Fee</p>' +
-                '<p class="annFeeValue" id="annFee-value' + index + '">' + creditCard.annualFee + '</p></div>');
 
-                index++;
-        });
-        $(function() {
+    function displayJsonData() {
+        var index = 1; //Iterates to give unique ID's and classes to certain elements
+        $.getJSON('cards.json', function(data) {
+            var accHeader;
+            switch (sortByOption) {
+                case "Cashback":
+                    data = $(data).sort(sortJsonByCashback);
+                    break;
+                case "AnnualFee":
+                    data = $(data).sort(sortJsonByAnnualFee);
+                    break;
+                default:
+                    data = $(data).sort(sortJsonByAPR);
+                    break;
+            }
+            $.each(data, function(i, creditCard) {
+                //Insert credit card name and apr into accordion header
+                switch (sortByOption) {
+                    case "Cashback":
+                        $('#accordion').append('<h3 id="header' + index + '"><b><span class="creditCard-name">' + creditCard.name +
+                        '</span><span class="headerDisplay">£' + creditCard.cashback + ' Cashback</span></b></h3>');
+                        break;
+                    case "AnnualFee":
+                        $('#accordion').append('<h3 class="header"><b><span class="creditCard-name">' + creditCard.name +
+                        '</span><span class="headerDisplay">£' + creditCard.annualFee + ' Annual Fee</span></b></h3>');
+                        break;
+                    default:
+                        $('#accordion').append('<h3 id="header' + index + '"><b><span class="creditCard-name">' + creditCard.name +
+                        '</span><span class="headerDisplay">' + creditCard.apr + '% APR</span></b></h3>');
+                        break;
+
+                }
+                //Inserts credit card image, information an cashback into accordion body
+                $('#header' + index).after('<div class="accordionContent"><img src="/img/' + creditCard.code.toLowerCase() + '.png"/>' +
+                    '<p class="information">' + creditCard.information + '</p>' +
+                    '<p class="cashback" id="cashback-sign' + index + '">Cashback</p>' +
+                    '<p class="cashbackValue" id="cashback-value' + index + '">£' + creditCard.cashback + '</p>' +
+                    '<p class="aprAccBody" id="apr-sign' + index + '">APR</p>' +
+                    '<p class="aprAccBodyValue" id="apr-value' + index + '">' + creditCard.apr + '%</p>' +
+                    '<p class="annFee" id="annFee-sign' + index + '">Annual Fee</p>' +
+                    '<p class="annFeeValue" id="annFee-value' + index + '">' + creditCard.annualFee + '</p></div>');
+
+                    index++;
+            });
             //Initializes accordion
             $("#accordion").accordion();
             //Defines accordion custom header icons for default and active state (images from css source)
@@ -41,6 +66,18 @@ $(document).ready(function() {
                 'activeHeader': 'selectedIcon'
             });
         });
+    }
+
+    var sortByOption = "APR";
+    sortByOption = localStorage.getItem("sortByOption");
+    $('.sortBySelect').val(sortByOption);
+    displayJsonData(sortByOption);
+
+    //SortBy drop down functionality
+    $('.sortBySelect').on('change', function() {
+        sortByOption = this.value;
+        localStorage.setItem("sortByOption", sortByOption);
+        location.reload(true);
     });
 
     // Side tab functionality
@@ -65,19 +102,15 @@ $(document).ready(function() {
 
 
     $('#menu-button').click(function() {
-        if (isOpen) {
-            slideMenuClose();
-        } else {
-            slideMenuOpen();
-        }
+        isOpen ? slideMenuClose() : slideMenuOpen();
     });
 
     $(window).on('resize', function() {
         if (isOpen) {
-        $('.container').animate({left: "-=300px"}, 300);
-        $('#menu-div').animate({left: "-=0px"}, 300);
-        $('#menu-div').hide();
-        isOpen = false;
+            $('.container').animate({left: "-=300px"}, 300);
+            $('#menu-div').animate({left: "-=0px"}, 300);
+            $('#menu-div').hide();
+            isOpen = false;
         }
     });
 });
