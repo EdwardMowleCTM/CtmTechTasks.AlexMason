608b1d6a37a1aac1354e53bbb971befa0a9eac72
alexmason355 <alexmason369@gmail.com>
2016-04-26 16:20:41 +0100

    Task9/10(unfinished): string templates in place but 'data' global variable remaining undefined

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@
 $(document).ready(function() {
     var sortByOption = "APR";
     var accHeaderDisplay = "";
+    var data = [];
+
     //Function to sort the json data by lowest to highest chosen data display value
-    function sortJsonData(a, b) {
+    function sortCardData(a, b) {
         switch (sortByOption) {
             case "Cashback":
                 return a.cashback > b.cashback ? 1 : -1;
@@ -16,53 +18,78 @@ $(document).ready(function() {
                 return a.apr > b.apr ? 1 : -1;
         }
     }
-    function displaySortByOption(creditCard) {
-        switch (sortByOption) {
-            case "Cashback":
-                return accHeaderDisplay = '<span class="headerDisplay">£' + creditCard.cashback + ' Cashback</span></b></h3>';
-                break;
-            case "AnnualFee":
-                return accHeaderDisplay = '</span><span class="headerDisplay">£' + creditCard.annualFee + ' Annual Fee</span></b></h3>';
-                break;
-            default:
-                return accHeaderDisplay = '</span><span class="headerDisplay">' + creditCard.apr + '% APR</span></b></h3>';
-        }
-    }
 
-    function displayJsonData() {
-        var index = 1; //Iterates to give unique ID's to certain elements
-        $.getJSON('cards.json', function(data) {
-            data = $(data).sort(sortJsonData);
-            $.each(data, function(i, creditCard) {
-                var accHeader = '<h3 id="header' + index + '"><b><span class="creditCardName">' + creditCard.name + '</span>';
-                //Insert credit card name and apr into accordion header
-                accHeaderDisplay = displaySortByOption(creditCard);
-                $('#accordion').append(accHeader + accHeaderDisplay);
-                //Inserts credit card image, information into accordion body
-                $('#header' + index).after('<div class="accordionContent"><img src="/img/' + creditCard.code.toLowerCase() + '.png"/><p class="information">' + creditCard.information + '</p><div class="cashbackContainer"><label class="cashback">Cashback</label><p class="cashbackValue">£' + creditCard.cashback + '</p></div><div class="moreInfoContainer"><label class="aprAccBody">APR</label><p class="aprAccBodyValue">' + creditCard.apr + '%</p><label class="annFee">Annual Fee</label><p class="annFeeValue">£' + creditCard.annualFee + '</p></div></div>');
-                    index++;
-            });
-            //Initializes accordion
-            $("#accordion").accordion();
-            //Defines accordion custom header icons for default and active state (images from css source)
-            $("#accordion").accordion("option", "icons", {
-                'header': 'defaultIcon',
-                'activeHeader': 'selectedIcon'
-            });
+    function getCardData() {
+        $.getJSON('cards.json', function(cardData) {
+            data = $(cardData).sort(sortCardData);
         });
     }
 
+    function getFilterHeader(creditCard) {
+        switch (sortByOption) {
+            case "Cashback":
+                return accHeaderDisplay = `<span class="headerDisplay">£${creditCard.cashback} Cashback</span></b></h3>`;
+                break;
+            case "AnnualFee":
+                return accHeaderDisplay = `</span><span class="headerDisplay">£${creditCard.annualFee} Annual Fee</span></b></h3>`;
+                break;
+            default:
+                return accHeaderDisplay = `</span><span class="headerDisplay">${creditCard.apr}% APR</span></b></h3>`;
+        }
+    }
 
-    sortByOption = localStorage.getItem("sortByOption");
-    $('.sortBySelect').val(sortByOption);
-    displayJsonData(sortByOption);
+    function displayCards(cardData) {
+        var index = 1; //Iterates to give unique ID's to certain elements
+        $.each(cardData, function(i, creditCard) {
+            var accHeader = `<h3 id="header${index}"><b><span class="creditCardName">${creditCard.name}</span>`;
+            //Insert credit card name and apr into accordion header
+            accHeaderDisplay = getFilterHeader(creditCard);
+            $('#accordion').append(accHeader + accHeaderDisplay);
+            //Inserts credit card image, information into accordion body
+            $('#header' + index).after(`
+                <div class="accordionContent">
+                    <img src="/img/${creditCard.code.toLowerCase()}.png">
+                    <p class="information">${creditCard.information}</p>
+                    <div class="cashbackContainer">
+                        <label class="cashback">Cashback</label>
+                        <p class="cashbackValue">£${creditCard.cashback}</p>
+                    </div>
+                    <div class="moreInfoContainer">
+                        <label class="aprAccBody">APR</label>
+                        <p class="aprAccBodyValue">${creditCard.apr}%</p>
+                        <label class="annFee">Annual Fee</label>
+                        <p class="annFeeValue">£${creditCard.annualFee}</p>
+                    </div>
+                </div>
+            `);
+            index++;
+        });
+        //Initializes accordion
+        $("#accordion").accordion();
+        //Defines accordion custom header icons for default and active state (images from css source)
+        $("#accordion").accordion("option", "icons", {
+            'header': 'defaultIcon',
+            'activeHeader': 'selectedIcon'
+        });
+    }
+
+     getCardData();
+     displayCards(data);
 
     //SortBy drop down functionality
     $('.sortBySelect').on('change', function() {
+        // $('#accordion').empty();
         sortByOption = this.value;
-        localStorage.setItem("sortByOption", sortByOption);
-        location.reload(true);
+        data = $(data).sort(sortCardData);
+        displayCards(data);
     });
+
+
+
+
+
+
+
 
     // Side tab functionality
     var isOpen = false;
@@ -78,11 +105,11 @@ $(document).ready(function() {
         if (isOpen) {
             $('.container').animate({left: "-=300px"}, 300);
             $('#menu-div').animate({left: "-=0px"}, 300);
-            return (isOpen = false);
+            isOpen = false;
         } else {
             $('.container').animate({left: "+=300px"}, 300);
             $('#menu-div').animate({left: "+=0px"}, 300);
-            return (isOpen = true);
+            isOpen = true;
         }
     }
 
@@ -90,11 +117,11 @@ $(document).ready(function() {
         if (isOpen) {
             $('.container').animate({left: "-=200px"}, 300);
             $('#menu-div').animate({left: "-=0px"}, 300);
-            return (isOpen = false);
+            isOpen = false;
         } else {
             $('.container').animate({left: "+=200px"}, 300);
             $('#menu-div').animate({left: "+=0px"}, 300);
-            return (isOpen = true);
+            isOpen = true;
         }
     }
 
